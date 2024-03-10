e32f64e4ddc60c7a8ad31ae1a4ae992f1fb62f51
Djakxter <161122918+PierreSpiroskiEsma@users.noreply.github.com>
2024-03-10 19:22:03 +0100

    Update Essence Visualisation

--- Assets/Script_Essence_Praticle_Rotation.cs
+++ Assets/Script_Essence_Praticle_Rotation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Script_Essence_Praticle_Rotation : MonoBehaviour{
 
@@ -12,4 +13,36 @@ public class Script_Essence_Praticle_Rotation : MonoBehaviour{
         this.transform.Rotate(0, 0, this.transform.rotation.z + Rotation_Speed_Clean);
 
     }
+
+    void Essence_Color_Switch (int type) {
+
+        switch (type) {
+
+            case 1:
+                this.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+                this.GetComponent<Light2D>().color = new Color(255, 0, 0);
+            break;
+
+            case 2:
+                this.GetComponent<SpriteRenderer>().color = new Color(50, 235, 210);
+                this.GetComponent<Light2D>().color = new Color(50, 235, 210);
+            break;
+
+            case 3:
+                this.GetComponent<SpriteRenderer>().color = new Color(235, 200, 50);
+                this.GetComponent<Light2D>().color = new Color(235, 200, 50);
+            break;
+        }
+
+
+
+
+
+
+
+    }
+
+    
+
+   
 }
